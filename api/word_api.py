dfb7bf45af66aae6c7cea281f383df4b2d415c10
Rébecca <rebecca@codingtheweb.ca>
2025-03-03 19:48:44 -0500

    feat: add api endpoint to create word

--- api/word_api.py
+++ api/word_api.py
@@ -1,7 +1,7 @@
 import fastapi
 
 from models.word import Word
-from services.word_service import find_word_en
+from services.word_service import create_word, find_word_en
 
 router = fastapi.APIRouter()
 
@@ -19,3 +19,15 @@ async def word(word_en: str):
             {"error": f"Word {word_en.capitalize()} not found"}, status_code=404
         )
     return word
+
+
+@router.post("/api/words/create", response_model=Word)
+async def word(word: Word):
+    new_word = await create_word(
+        word_en=word.en,
+        word_fr=word.fr,
+        pictogram=word.pictogram,
+        asl_video=word.asl_video,
+        category=word.category,
+    )
+    return new_word
