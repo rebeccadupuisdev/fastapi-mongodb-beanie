48e7428705b376f49ad64d7b75f72fc89e624996
Rébecca <rebecca@codingtheweb.ca>
2025-03-03 19:12:27 -0500

    feat: add api endpoint to find en word

--- api/word_api.py
+++ api/word_api.py
@@ -1,8 +1,21 @@
 import fastapi
 
+from models.word import Word
+from services.word_service import find_word_en
+
 router = fastapi.APIRouter()
 
 
 @router.get("/api")
 def api():
-    return {"Hello API!"}
+    return {"message": "Hello API!"}
+
+
+@router.get("/api/words/en/{word_en}", response_model=Word)
+async def word(word_en: str):
+    word = await find_word_en(word_en)
+    if not word:
+        return fastapi.responses.JSONResponse(
+            {"error": f"Word {word_en.capitalize()} not found"}, status_code=404
+        )
+    return word
