a41a26be926f7e134155566760c27b11c6635fb2
Rébecca <info@rebeccadupuis.ca>
2025-03-28 14:33:04 -0400

    refactor: add pydantic Field to WordCreate attributes

--- models/word.py
+++ models/word.py
@@ -1,13 +1,19 @@
 from beanie import Document
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, Field, HttpUrl
 
 
 class WordCreate(BaseModel):
-    en: str
-    fr: str
-    pictogram: HttpUrl
-    asl_video: HttpUrl
-    category: str | None = None
+    en: str = Field(..., title="Word in english", example="Cheese")
+    fr: str = Field(..., title="Word in french", example="Fromage")
+    pictogram: HttpUrl = Field(
+        ...,
+        title="Link of the pictogram image",
+        example="http://example.com/cheese.jpg",
+    )
+    asl_video: HttpUrl = Field(
+        ..., title="Link of the ASL video", example="http://example.com/cheese.mp4"
+    )
+    category: str | None = Field(None, title="Category for the word", example="Food")
 
 
 class Word(WordCreate, Document):
