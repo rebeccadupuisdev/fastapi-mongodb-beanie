21a1624f151d23aa33a85b5da4591828e7b05610
Rébecca <info@rebeccadupuis.ca>
2025-04-26 10:46:03 -0400

    test: add test for get_words

--- tests/services/test_word_service.py
+++ tests/services/test_word_service.py
@@ -1,8 +1,9 @@
 import pytest
+import pytest_asyncio
 from pydantic import HttpUrl
 
 from models.word import WordCreate
-from services.word_service import create_word
+from services.word_service import create_word, get_words
 
 
 @pytest.mark.asyncio(loop_scope="session")
@@ -23,3 +24,54 @@ async def test_create_word():
     assert new_word.pictogram == HttpUrl("http://example.com/image.jpg")
     assert new_word.asl_video == HttpUrl("http://example.com/video.mp4")
     assert new_word.category is None
+
+
+@pytest_asyncio.fixture
+async def base_data():
+
+    data = [
+        {
+            "en": "Cheese",
+            "fr": "Fromage",
+            "pictogram": "http://example.com/image.jpg",
+            "asl_video": "http://example.com/video.mp4",
+        },
+        {
+            "en": "Ice Cream",
+            "fr": "Crème Glacée",
+            "pictogram": "http://example.com/image.jpg",
+            "asl_video": "http://example.com/video.mp4",
+        },
+        {
+            "en": "Water",
+            "fr": "Eau",
+            "pictogram": "http://example.com/image.jpg",
+            "asl_video": "http://example.com/video.mp4",
+        },
+    ]
+
+    for item in data:
+
+        w = WordCreate(
+            en=item.get("en"),
+            fr=item.get("fr"),
+            pictogram=item.get("pictogram"),
+            asl_video=item.get("asl_video"),
+        )
+
+        await create_word(**w.model_dump())
+
+    return data
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_get_words(base_data):
+    """Test getting all items from the database"""
+
+    words = await get_words()
+
+    for i, word in enumerate(words):
+        assert word.en == base_data[i].get("en")
+        assert word.fr == base_data[i].get("fr")
+
+    assert len(words) == len(base_data)
